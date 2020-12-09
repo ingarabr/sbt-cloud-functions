c020811cf1cfe7c95653eba81b3e0b8e4971b798
Ingar Abrahamsen <ingar@abrahams1.com>
2020-12-09 23:38:05 +0100

    feat: run gcp function locally

--- src/main/scala/com/github/ingarabr/GcpFunctionKeys.scala
+++ src/main/scala/com/github/ingarabr/GcpFunctionKeys.scala
@@ -1,2 +1,20 @@
 package com.github.ingarabr
-class GcpFunctionKeys {}
+
+import sbt.{settingKey, taskKey}
+
+trait GcpFunctionKeys {
+  val gcpFunctionClass =
+    taskKey[String]("The function class to use.")
+
+  val gcpFunctionPort =
+    taskKey[Int]("The http port to server the function on.")
+
+  val gcpFunctionRunLocally =
+    taskKey[Unit]("Run a cloud function locally")
+
+  val gcpFunctionInvokerVersion =
+    settingKey[String]("java-function-invoker version")
+
+  val gcpFunctionFrameworkApiVersion =
+    settingKey[String]("functions-framework-api version")
+}
